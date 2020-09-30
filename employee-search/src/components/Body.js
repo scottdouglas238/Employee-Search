83907cb414b9b00eeb344a182e36fb4ef87e1f37
Scott <scottdouglas238@gmail.com>
2020-09-29 23:05:52 -0700

    added mock phont

--- employee-search/src/components/Body.js
+++ employee-search/src/components/Body.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../styles/Body.css";
+const image = "https://media.phillyvoice.com/media/images/11222017_Anthony_Green.2e16d0ba.fill-735x490.jpg"
+
 
 function Body() {
     return (
@@ -26,11 +28,11 @@ function Body() {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>9/10/1987</td>
+    <th scope="row"><img className = "personImage" src = {image}/></th>
+      <td>Anthony Green</td>
+      <td>555-123-1234</td>
+      <td>circaSurvive@yahoo.com</td>
+      <td>9/10/1985</td>
     </tr>
     
     
