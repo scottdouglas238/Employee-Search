dd5e534d9667c288a64f5c8ab15c88fadd61355e
Scott <scottdouglas238@gmail.com>
2020-10-01 00:02:43 -0700

    Table data refined

--- employee-search/src/App.js
+++ employee-search/src/App.js
@@ -10,39 +10,39 @@ class App extends Component {
     search: ""
   };
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <Header />
         <SearchName />
-        <div class = "container">
-        <div class = "row"> 
-        <table className = "table table-striped">
-        <thread>
-        <tr>
-        <th scope="col">Image</th>
-        <th scope="col">Name</th>
-        <th scope="col">Phone</th>
-        <th scope="col">Email</th>
-        <th scope="col">DOB</th>
-        </tr>
-    </thread>
-    <tbody>
-        {this.state.employees.map(employee => (
-        <tr>
-        <Table 
-        id={employee.id}
-        img={employee.image}
-        name={employee.name}
-        phone={employee.phone}
-        email={employee.email}
-        dob={employee.dob}
-        />
-        </tr>
-        ))}
-        </tbody>
-        </table>
-        </div> 
+        <div className="container">
+          <div className="row">
+            <table className="table table-striped ">
+              <thead>
+                <tr>
+                  <th scope="col">Image</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Phone</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">DOB</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.employees.map(employee => (
+                  <tr>
+                    <Table
+                      id={employee.id}
+                      img={employee.image}
+                      name={employee.name}
+                      phone={employee.phone}
+                      email={employee.email}
+                      dob={employee.dob}
+                    />
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     )
--- employee-search/src/components/Table.js
+++ employee-search/src/components/Table.js
@@ -4,14 +4,14 @@ import "../styles/table.css";
 function Table(props) {
     console.log(props)
     return (
-        <table className="table table-striped">
-            <td scope="row"><img className="personImage" alt={props.name} src={props.img} /></td>
+        <>
+            <th scope="row"><img className="personImage" alt={props.name} src={props.img} /></th>
             <td>{props.name}</td>
             <td>{props.phone}</td>
             <td>{props.email}</td>
             <td>{props.dob}</td>
-        </table>
-)
+        </>
+    )
 }
 
 export default Table;
